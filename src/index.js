a89b5d30b04f526025ff1f05a6d7efcd72d73a99
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2022-10-09 20:23:09 -0400

    add nodemon and use worker_thread

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-const process_name = process.argv.slice(2)[0];
+const express = require("express");
+const { Worker } = require("worker_threads");
 
-count = 0;
-while (true) {
-  count++;
-  if (count == 2000 || count == 4000) {
-    console.log(`${process_name}: ${count}`);
-  }
+const app = express();
+const port = process.env.PORT || 3000;
+const THREAD_COUNT = 8;
+
+function createWorker() {
+  return new Promise(function (resolve, reject) {
+    const worker = new Worker("./src/worker.js", {
+      workerData: { thread_count: THREAD_COUNT },
+    });
+
+    worker.on("message", (data) => {
+      resolve(data);
+    });
+    worker.on("error", (msg) => {
+      reject(`An error ocurred: ${msg}`);
+    });
+  });
 }
+
+app.get("/non-blocking/", (req, res) => {
+  res.status(200).send("This page is non-blocking");
+});
+
+function calculateCount() {
+  return new Promise((resolve, reject) => {
+    let counter = 0;
+    for (let i = 0; i < 20_000_000_000; i++) {
+      counter++;
+    }
+    resolve(counter);
+  });
+}
+
+app.get("/old-blocking", async (req, res) => {
+  const counter = await calculateCount();
+  res.status(200).send(`result is ${counter}`);
+});
+
+app.get("/blocking", async (req, res) => {
+  const workerPromises = [];
+  for (let i = 0; i < THREAD_COUNT; i++) {
+    workerPromises.push(createWorker());
+  }
+  const thread_results = await Promise.all(workerPromises);
+  const total = thread_results.reduce(
+    (previousValue, currentValue) => previousValue + currentValue,
+    0
+  );
+  res.status(200).send(`result is ${total}`);
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
